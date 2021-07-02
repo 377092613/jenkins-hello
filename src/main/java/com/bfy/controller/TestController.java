c1ab5fb9e563e882e4882317da3fd2e818bc8159
LAPTOP-TV0QKE0P\lenovo <377092613@qq.com>
2021-07-02 13:57:06 +0800

    初始化项目,并完成controller

---
+++ src/main/java/com/bfy/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.bfy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String test(){
+        return "哈哈哈哈";
+    }
+}
